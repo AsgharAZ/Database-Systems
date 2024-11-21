cf89e601ac9e2a4340826991d97bc984ea0adedf
Asghar Abbas <s.aazaidi2001@gmail.com>
2024-11-21 13:02:58 +0500

    Week 13 and Quiz 5

---
+++ Homeworks/Homework 2/template_hw02.sql
@@ -0,0 +1,118 @@
+-- Q1
+-- List the number of employees in each department.
+-- Result contains 3 rows.
+-- Write your explanation here (Replace this comment with your explanation). 
+-- Also replace the below given query with your query. Don't add additional lines or change this format.
+select * from Bonus
+
+-- Q2
+-- Display the employment history of worker ’Malik Zain’; i.e. list all his designations where current designation must be at the top and the first designation in the company must be at the bottom (ignore future designations).
+-- Result contains 3 rows
+-- Write your explanation here (Replace this comment with your explanation). 
+-- Also replace the below given query with your query. Don't add additional lines or change this format.
+select * from Bonus 
+
+-- Q3
+-- Display the total salary of all workers belonging to the department ’Admin’.
+-- Result contains 1 row
+-- Write your explanation here (Replace this comment with your explanation). 
+-- Also replace the below given query with your query. Don't add additional lines or change this format.
+select * from Bonus
+
+-- Q4
+-- List all workers with their salaries and department name. If a worker has not been assigned any department, then the result must show ’No Department’ for those workers.
+-- Result contains 10 rows
+-- Write your explanation here (Replace this comment with your explanation). 
+-- Also replace the below given query with your query. Don't add additional lines or change this format.
+select * from Bonus
+
+-- Q5
+-- List all workers with their joining dates (without time) and the total number of years of service in the company.
+-- Result contains 1 rows
+-- Write your explanation here (Replace this comment with your explanation). 
+-- Also replace the below given query with your query. Don't add additional lines or change this format.
+select * from Bonus
+
+-- Q6 
+-- List all records of the ’Worker’ entity
+-- Result contains 10 rows
+-- Write your explanation here (Replace this comment with your explanation). 
+-- Also replace the below given query with your query. Don't add additional lines or change this format.
+select * from Bonus
+
+-- Q7
+-- List all the workers who do not have any designation and also are not assigned to any department.
+-- Result contains 2 rows
+-- Write your explanation here (Replace this comment with your explanation). 
+-- Also replace the below given query with your query. Don't add additional lines or change this format.
+select * from Bonus
+
+-- Q8
+-- List department names with the highest salary of the department
+-- Result contains 3 rows
+-- Write your explanation here (Replace this comment with your explanation). 
+-- Also replace the below given query with your query. Don't add additional lines or change this format.
+select * from Bonus
+
+-- Q9
+-- List all workers with the current designation order by First Name.
+-- Result contains 8 rows
+-- Write your explanation here (Replace this comment with your explanation). 
+-- Also replace the below given query with your query. Don't add additional lines or change this format.
+select * from Bonus
+
+-- Q10
+-- List all departments which have no workers assigned to them.
+-- Result contains 1 row
+-- Write your explanation here (Replace this comment with your explanation). 
+-- Also replace the below given query with your query. Don't add additional lines or change this format.
+select * from Bonus
+
+-- Q11
+-- List workers joined on the same date - not necessary at the same time (An employee should not be listed if there is no other employee hired on the same date). You have to show new employees first.
+-- Result contains 8 rows.
+-- Write your explanation here (Replace this comment with your explanation). 
+-- Also replace the below given query with your query. Don't add additional lines or change this format.
+select * from Bonus
+
+-- Q12 
+-- List all workers who never got any bonuses. Your result must be sorted with respect to their names.
+-- Result contains 4 rows
+-- Write your explanation here (Replace this comment with your explanation). 
+-- Also replace the below given query with your query. Don't add additional lines or change this format.
+select * from Bonus
+
+-- Q13
+-- List names of all workers getting salaries more than the average salaries of their department
+-- Result contains 4 rows
+-- Write your explanation here (Replace this comment with your explanation). 
+-- Also replace the below given query with your query. Don't add additional lines or change this format.
+select * from Bonus
+
+-- Q14 
+-- List all workers with the total bonus they have received. If a worker has not received any bonus then it must show 0 for that worker. Your result must be sorted in a decreasing amount of total bonus
+-- Result contains 10 rows
+-- Write your explanation here (Replace this comment with your explanation). 
+-- Also replace the below given query with your query. Don't add additional lines or change this format.
+select * from Bonus
+
+-- Q15
+-- List all workers who received more than one bonus in a year (regardless of which year).
+-- Result contains 2 rows
+-- Write your explanation here (Replace this comment with your explanation). 
+-- Also replace the below given query with your query. Don't add additional lines or change this format.
+select * from Bonus
+
+-- Q16
+-- List workers earning the second-highest salary in the organization.
+-- Result contains 2 rows
+-- Write your explanation here (Replace this comment with your explanation). 
+-- Also replace the below given query with your query. Don't add additional lines or change this format.
+select * from Bonus
+
+-- Q17
+-- List all workers who received bonuses in the year 2017 but not in the year 2018 along with the total bonus amount (regardless of year)
+-- Result contains 3 rows
+-- Write your explanation here (Replace this comment with your explanation). 
+-- Also replace the below given query with your query. Don't add additional lines or change this format.
+select * from Bonus
